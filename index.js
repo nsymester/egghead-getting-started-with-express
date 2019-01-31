baad02effb8a29e67c25fd5d86d946fde7bf6a1a
neil.symester@gmail.com <neil.symester@gmail.com>
2019-01-31 23:36:13 +0000

    Dynamic routes

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const express = require("express");
-
 const app = express();
+
+const fs = require("fs");
+const _ = require("lodash");
+let users = [];
+
+fs.readFile("users.json", { encoding: "utf8" }, function(err, data) {
+  if (err) throw err;
+
+  JSON.parse(data).forEach(function(user) {
+    user.name.full = _.startCase(`${user.name.first} ${user.name.last}`);
+    users.push(user);
+  });
+});
 
 // req = request
 // res = result
 app.get("/", function(req, res) {
-  res.send("Hello world");
+  let buffer = "";
+
+  users.forEach(function(user) {
+    buffer += `<a href="/${user.name.full}">${user.name.full}</a> <br/>`;
+  });
+  res.send(buffer);
 });
 
-app.get("/yo", function(req, res) {
-  res.send("YO");
+// define a route matching a regular expression
+app.get(/big.*/, function(req, res, next) {
+  console.log("BIG USER ACCESS");
+  // next() passes control onto the next control handler
+  next();
 });
 
-var server = app.listen(3000, function() {
+app.get(/.*dog.*/, function(req, res, next) {
+  console.log("DOGS GO WOOF");
+  next();
+});
+
+// :usernmae - the colon tells express that its a path variable
+app.get("/:username", function(req, res) {
+  let username = req.params.username;
+  res.send(username);
+});
+
+const server = app.listen(3000, function() {
   console.log(`Server at http://localhost:${server.address().port}`);
 });
